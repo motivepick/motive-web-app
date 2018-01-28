0ef9122bcae5f6a135ccea59dd80920e09f681b2
yaskovdev <yaskovdev@gmail.com>
2018-01-28 16:16:56 +0200

    Call the local server.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            tasks: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8080/tasks")
+            .then(response => response.json())
+            .then(
+                (json) => {
+                    this.setState({
+                        isLoaded: true,
+                        tasks: json
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const {error, isLoaded, tasks} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">Welcome to React</h1>
+                    </header>
+                    <ul className="App-intro">
+                        {tasks.map(task => (
+                            <li key={task.name}>
+                                {task.name}: {task.description}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
