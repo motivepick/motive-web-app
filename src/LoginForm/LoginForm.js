b8af87bcb05749cc5dd41ac6aa80e54b229dc9b2
yaskovdev <yaskovdev@gmail.com>
2018-06-09 14:26:02 +0300

    Try with fetch.

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -6,18 +6,17 @@ class LoginForm extends Component {
         const query = new URLSearchParams(this.props.location.search);
 
         const login = () => {
+            const form = new FormData();
+            form.append('clientId', '72d4b07903134b5aaa52');
+            form.append('clientSecret', 'b54153a2a693e3d8ea9f012c863af8706f6fc93c');
+            form.append('code', query.get('code'));
+            form.append('redirect_uri', 'https://motiv.yaskovdev.com');
             fetch('https://github.com/login/oauth/access_token', {
-                method: 'POST',
+                method: 'post',
                 headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+                    'Accept': 'application/json'
                 },
-                body: JSON.stringify({
-                    clientId: '72d4b07903134b5aaa52',
-                    clientSecret: 'b54153a2a693e3d8ea9f012c863af8706f6fc93c',
-                    code: query.get('code'),
-                    redirect_uri: 'https://motiv.yaskovdev.com'
-                })
+                body: form
             })
                 .then(response => response.json())
                 .then(
