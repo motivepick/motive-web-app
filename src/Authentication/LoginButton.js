54c4bdaa91b2e800c1dfc9c689e1cf7bd48aef57
Jelena <jelena.shaipuka@gmail.com>
2018-06-19 21:54:25 +0300

    Local dev

--- src/Authentication/LoginButton.js
+++ src/Authentication/LoginButton.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import {Button} from 'reactstrap';
+import {APP_URL, FACEBOOK_CLIENT_ID} from '../const';
 
 class LoginButton extends Component {
 
     render() {
-        const clientId = '2066377776913735';
-        const redirectUrl = encodeURI('https://motiv.yaskovdev.com/login');
-        const url = `https://www.facebook.com/v3.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUrl}&state=123`;
+        const redirectUrl = encodeURI(`${APP_URL}/login`);
+        const url = `https://www.facebook.com/v3.0/dialog/oauth?client_id=${FACEBOOK_CLIENT_ID}&redirect_uri=${redirectUrl}&state=123`;
         return (
             <Button href={url}>Login With Facebook</Button>
         );
