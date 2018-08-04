1c72c8d45a25d5a945f0dd39fe8b21fb57a51ed3
yaskovdev <yaskovdev@gmail.com>
2018-08-04 18:02:28 +0300

    Add recognition for days of weeks #27

--- src/Tasks/parser.js
+++ src/Tasks/parser.js
@@ -1,19 +1,47 @@
 import moment from 'moment';
 
+
+const DAYS_OF_WEEK = {
+    'Monday': 'понедельник',
+    'Tuesday': 'вторник',
+    'Wednesday': 'среду',
+    'Thursday': 'четверг',
+    'Friday': 'пятницу',
+    'Saturday': 'субботу',
+    'Sunday': 'воскресенье'
+};
+
+const ALL_DAYS_OF_WEEK = Object.keys(DAYS_OF_WEEK) + Object.values(DAYS_OF_WEEK);
+
 export const handleDueDateOf = (task) => {
-    const lastWord = lastWordOf(task.name);
+    const words = wordsOf(task.name);
+    const lastWord = words[words.length - 1];
+    const wordBeforeLast = words.length > 1 ? words[words.length - 2] : null;
     if (['today', 'сегодня'].includes(lastWord)) {
-        return { ...task, name: nameWithoutLastWord(task, lastWord), dueDate: moment().endOf('day') };
+        return { ...task, name: nameWithoutLastWord(task.name, lastWord), dueDate: moment().endOf('day') };
     } else if (['tomorrow', 'завтра'].includes(lastWord)) {
         return {
-            ...task, name: nameWithoutLastWord(task, lastWord), dueDate: moment().add(1, 'days').endOf('day')
+            ...task, name: nameWithoutLastWord(task.name, lastWord), dueDate: moment().add(1, 'days').endOf('day')
         };
     } else {
-        const date = moment(lastWord, ['DD.MM.YYYY', 'DD.MM.YY'], true).endOf('day');
-        return date.isValid() ? { ...task, name: nameWithoutLastWord(task, lastWord), dueDate: date.endOf('day') } : { ...task };
+        if (ALL_DAYS_OF_WEEK.includes(lastWord) && ['on', 'в', 'во'].includes(wordBeforeLast)) {
+            const dayOfWeek = Object.keys(DAYS_OF_WEEK).includes(lastWord)
+                ? lastWord : Object.entries(DAYS_OF_WEEK).filter(entry => entry[1] === lastWord)[0][0];
+            const startOfToday = moment().startOf('day');
+            const dueDate = moment().day(dayOfWeek).endOf('day');
+            const dueDateInFuture = dueDate.isBefore(startOfToday) ? dueDate.add(1, 'weeks') : dueDate;
+            return {
+                ...task,
+                name: nameWithoutLastWord(nameWithoutLastWord(task.name, lastWord), wordBeforeLast),
+                dueDate: dueDateInFuture
+            };
+        } else {
+            const date = moment(lastWord, ['DD.MM.YYYY', 'DD.MM.YY'], true).endOf('day');
+            return date.isValid() ? { ...task, name: nameWithoutLastWord(task.name, lastWord), dueDate: date.endOf('day') } : { ...task };
+        }
     }
 };
 
-const nameWithoutLastWord = (task, lastWord) => task.name.substring(0, task.name.length - lastWord.length).trim();
+const nameWithoutLastWord = (name, lastWord) => name.substring(0, name.length - lastWord.length).trim();
 
-const lastWordOf = (name) => name.split(' ').splice(-1)[0].toLowerCase().trim();
+const wordsOf = (name) => name.split(' ').map(w => w.trim());
