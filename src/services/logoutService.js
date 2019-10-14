e8d5c4f2efbfe937a85b1804ce300e971bc60b62
yaskovdev <yaskovdev@gmail.com>
2019-10-14 18:38:23 +0300

    Cleanup.

--- src/services/logoutService.js
+++
@@ -1,4 +0,0 @@
-import request from 'superagent'
-import { API_URL } from '../const'
-
-export const logout = async () => await request.post(`${API_URL}/logout`)
--- src/utils/exceptionHandler.js
+++ src/utils/exceptionHandler.js
@@ -1,9 +1,21 @@
 import { history } from '../index'
 
+const handleServerError = () => {
+}
+
+const handleNoResponseFromServer = () => {
+}
+
 export const handleServerException = (e) => {
     const { response } = e
-    const { status } = response
-    if (status === 401 || status === 403) {
-        history.push('/login')
+    if (response) {
+        const { status } = response
+        if (status === 401 || status === 403) {
+            history.push('/login')
+        } else {
+            handleServerError()
+        }
+    } else {
+        handleNoResponseFromServer()
     }
 }
