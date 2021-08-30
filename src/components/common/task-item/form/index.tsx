45a55edfb744ae7e71b685d62ecc4518ae185154
jelena <jelenashaipuka@gmail.com>
2021-08-30 18:22:35 +0300

    useTranslation hook

--- src/components/common/task-item/form/index.tsx
+++ src/components/common/task-item/form/index.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent } from 'react'
-import { withTranslation, WithTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import { Col, Form, FormGroup, Row } from 'reactstrap'
 import { TASK_DESCRIPTION_LIMIT, TASK_NAME_LIMIT } from '../../../../config'
 import { ITask } from '../../../../models/appModel'
@@ -7,24 +7,33 @@ import { CustomInput } from '../../../Tasks/CustomInput'
 
 import './styles.css'
 
-interface TaskFormProps extends WithTranslation {
+interface TaskFormProps {
     task?: ITask;
     saveName: (name: string) => string;
     saveDescription: (description: string) => string;
 }
 
 const TaskForm: React.FC<TaskFormProps> = props => {
-    const { task, saveName, saveDescription, t } = props
+    const { task, saveName, saveDescription } = props
+    const { t } = useTranslation()
     return (
         <Row className="detailed">
             <Col>
                 <Form className="task-form" onSubmit={(e: FormEvent<HTMLFormElement>) => e.preventDefault()}>
                     <FormGroup>
-                        <CustomInput type="text" value={task?.name} onSave={saveName} maxLength={TASK_NAME_LIMIT}/>
+                        <CustomInput
+                            type="text"
+                            value={task?.name}
+                            onSave={saveName}
+                            maxLength={TASK_NAME_LIMIT}/>
                     </FormGroup>
                     <FormGroup className="task-form-description">
-                        <CustomInput type="textarea" placeholder={t('task.description')} value={task?.description}
-                                     onSave={saveDescription} maxLength={TASK_DESCRIPTION_LIMIT}/>
+                        <CustomInput
+                            type="textarea"
+                            value={task?.description}
+                            onSave={saveDescription}
+                            maxLength={TASK_DESCRIPTION_LIMIT}
+                            placeholder={t('task.description')}/>
                     </FormGroup>
                 </Form>
             </Col>
@@ -32,4 +41,4 @@ const TaskForm: React.FC<TaskFormProps> = props => {
     )
 }
 
-export default withTranslation()(TaskForm)
+export default TaskForm
